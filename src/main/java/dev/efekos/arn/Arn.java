e73a54b6c9a667ec22ca50dd85c5c4f835614582
efekos <56752400+efekos@users.noreply.github.com>
2024-05-16 00:46:16 +0300

    a bunch of error checks

--- src/main/java/dev/efekos/arn/Arn.java
+++ src/main/java/dev/efekos/arn/Arn.java
@@ -2,6 +2,7 @@ package dev.efekos.arn;
 
 import com.mojang.brigadier.CommandDispatcher;
 import dev.efekos.arn.annotation.Command;
+import dev.efekos.arn.annotation.CommandAnnotationData;
 import dev.efekos.arn.annotation.Container;
 import dev.efekos.arn.annotation.RestCommand;
 import dev.efekos.arn.config.ArnConfigurer;
@@ -11,11 +12,16 @@ import dev.efekos.arn.handler.CommandHandlerMethod;
 import dev.efekos.arn.resolver.CommandArgumentResolver;
 import net.minecraft.commands.CommandListenerWrapper;
 import net.minecraft.server.MinecraftServer;
+import org.bukkit.command.BlockCommandSender;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
 import org.reflections.Reflections;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 import java.util.*;
 
 public final class Arn {
@@ -93,16 +99,36 @@ public final class Arn {
         //TODO
     }
 
+    private static final List<Class<? extends CommandSender>> REQUIRED_SENDER_CLASSES = Arrays.asList(CommandSender.class, Player.class, ConsoleCommandSender.class, BlockCommandSender.class);
+
     private void command(Command annotation,Method method) throws ArnCommandException{
         if(handlers.containsKey(annotation.value())) throw new ArnCommandException("Duplicate command '" + annotation.value() + "'");
+        if(!method.getReturnType().equals(int.class)) throw new ArnCommandException("Handler method '"+ method.getName() + "' for command '" + annotation.value() + "' does not return 'int'");
+        long count = Arrays.stream(method.getParameters()).filter(parameter -> REQUIRED_SENDER_CLASSES.contains(parameter.getType())).count();
+        if(count!=1) throw new ArnCommandException("Handler method '"+ method.getName() + "' for command '" + annotation.value() + "' must contain exactly one parameter that is a CommandSender.");
+        for (Parameter parameter : method.getParameters()) {
+            if(resolvers.stream().noneMatch(car -> car.isApplicable(parameter))) throw new ArnCommandException("Handler method '"+ method.getName() + "' for command '" + annotation.value() + "' has a parameter '"+parameter.getName()+"' that isn't applicable for anything.");
+        }
 
+        CommandHandlerMethod commandHandlerMethod = createHandlerMethod(annotation, method);
+
+        handlers.put(annotation.value(), commandHandlerMethod);
+    }
+
+    private static CommandHandlerMethod createHandlerMethod(Command annotation, Method method) {
         CommandHandlerMethod commandHandlerMethod = new CommandHandlerMethod();
 
         commandHandlerMethod.setCommand(annotation.value());
         commandHandlerMethod.setMethod(method);
         commandHandlerMethod.setParameters(Arrays.asList(method.getParameters()));
 
-        handlers.put(annotation.value(), commandHandlerMethod);
+        CommandAnnotationData baseAnnData = new CommandAnnotationData(annotation);
+
+        if(baseAnnData.getDescription()==null) baseAnnData.setDescription("No description provided.");
+        if(baseAnnData.getPermission()==null) baseAnnData.setPermission("spigot.command."+ annotation.value());
+
+        commandHandlerMethod.setAnnotationData(baseAnnData);
+        return commandHandlerMethod;
     }
 
     private void registerCommands(){
