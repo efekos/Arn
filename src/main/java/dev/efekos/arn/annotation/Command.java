f05bd860c5ab0e48b445626963648abee54b3cdb
efekos <56752400+efekos@users.noreply.github.com>
2024-05-15 07:56:43 +0300

    add retention & target to annotations

--- src/main/java/dev/efekos/arn/annotation/Command.java
+++ src/main/java/dev/efekos/arn/annotation/Command.java
@@ -1,5 +1,12 @@
 package dev.efekos.arn.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface Command {
     String value() default "";
     String description() default "";
--- src/main/java/dev/efekos/arn/annotation/CommandArgument.java
+++ src/main/java/dev/efekos/arn/annotation/CommandArgument.java
@@ -1,5 +1,13 @@
 package dev.efekos.arn.annotation;
 
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER)
 public @interface CommandArgument {
     String value() default "";
     boolean required() default true;
