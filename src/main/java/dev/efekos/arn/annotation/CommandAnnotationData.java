3304805859e10b8c3534da04eb6ce2db75da0628
efekos <56752400+efekos@users.noreply.github.com>
2024-05-16 19:19:42 +0300

    add maven-sources-plugin and remove lombok

--- src/main/java/dev/efekos/arn/annotation/CommandAnnotationData.java
+++ src/main/java/dev/efekos/arn/annotation/CommandAnnotationData.java
@@ -1,12 +1,10 @@
 package dev.efekos.arn.annotation;
 
-import lombok.Data;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
-@Data
 public class CommandAnnotationData {
     private String name;
     private List<String> aliases;
@@ -18,5 +16,60 @@ public class CommandAnnotationData {
         setDescription(ann.description());
         setPermission(ann.permission());
         setAliases(ann.aliases()!=null? Arrays.asList(ann.aliases())  :new ArrayList<>());
+    }
+
+    @Override
+    public String toString() {
+        return "CommandAnnotationData{" +
+                "name='" + name + '\'' +
+                ", aliases=" + aliases +
+                ", description='" + description + '\'' +
+                ", permission='" + permission + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CommandAnnotationData that = (CommandAnnotationData) o;
+        return Objects.equals(name, that.name) && Objects.equals(aliases, that.aliases) && Objects.equals(description, that.description) && Objects.equals(permission, that.permission);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, aliases, description, permission);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getAliases() {
+        return aliases;
+    }
+
+    public void setAliases(List<String> aliases) {
+        this.aliases = aliases;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPermission() {
+        return permission;
+    }
+
+    public void setPermission(String permission) {
+        this.permission = permission;
     }
 }
--- src/main/java/dev/efekos/arn/handler/CommandHandlerMethod.java
+++ src/main/java/dev/efekos/arn/handler/CommandHandlerMethod.java
@@ -3,13 +3,12 @@ package dev.efekos.arn.handler;
 import dev.efekos.arn.annotation.CommandAnnotationData;
 import dev.efekos.arn.resolver.CommandArgumentResolver;
 import dev.efekos.arn.resolver.CommandHandlerMethodArgumentResolver;
-import lombok.Data;
 
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
 import java.util.List;
+import java.util.Objects;
 
-@Data
 public class CommandHandlerMethod {
     private String command;
     private Method method;
@@ -17,4 +16,77 @@ public class CommandHandlerMethod {
     private List<Parameter> parameters;
     private List<CommandArgumentResolver> argumentResolvers;
     private List<CommandHandlerMethodArgumentResolver> handlerMethodResolvers;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CommandHandlerMethod that = (CommandHandlerMethod) o;
+        return Objects.equals(command, that.command) && Objects.equals(method, that.method) && Objects.equals(annotationData, that.annotationData) && Objects.equals(parameters, that.parameters) && Objects.equals(argumentResolvers, that.argumentResolvers) && Objects.equals(handlerMethodResolvers, that.handlerMethodResolvers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(command, method, annotationData, parameters, argumentResolvers, handlerMethodResolvers);
+    }
+
+    @Override
+    public String toString() {
+        return "CommandHandlerMethod{" +
+                "command='" + command + '\'' +
+                ", method=" + method +
+                ", annotationData=" + annotationData +
+                ", parameters=" + parameters +
+                ", argumentResolvers=" + argumentResolvers +
+                ", handlerMethodResolvers=" + handlerMethodResolvers +
+                '}';
+    }
+
+    public String getCommand() {
+        return command;
+    }
+
+    public void setCommand(String command) {
+        this.command = command;
+    }
+
+    public Method getMethod() {
+        return method;
+    }
+
+    public void setMethod(Method method) {
+        this.method = method;
+    }
+
+    public CommandAnnotationData getAnnotationData() {
+        return annotationData;
+    }
+
+    public void setAnnotationData(CommandAnnotationData annotationData) {
+        this.annotationData = annotationData;
+    }
+
+    public List<Parameter> getParameters() {
+        return parameters;
+    }
+
+    public void setParameters(List<Parameter> parameters) {
+        this.parameters = parameters;
+    }
+
+    public List<CommandArgumentResolver> getArgumentResolvers() {
+        return argumentResolvers;
+    }
+
+    public void setArgumentResolvers(List<CommandArgumentResolver> argumentResolvers) {
+        this.argumentResolvers = argumentResolvers;
+    }
+
+    public List<CommandHandlerMethodArgumentResolver> getHandlerMethodResolvers() {
+        return handlerMethodResolvers;
+    }
+
+    public void setHandlerMethodResolvers(List<CommandHandlerMethodArgumentResolver> handlerMethodResolvers) {
+        this.handlerMethodResolvers = handlerMethodResolvers;
+    }
 }
