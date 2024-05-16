4aed3c9e7565a65f2b7c143c4bed522581ffe640
efekos <56752400+efekos@users.noreply.github.com>
2024-05-17 00:14:23 +0300

    allow CommandSyntaxExceptions as they are an important part of Minecraft's error handling

--- src/main/java/dev/efekos/arn/resolver/CommandHandlerMethodArgumentResolver.java
+++ src/main/java/dev/efekos/arn/resolver/CommandHandlerMethodArgumentResolver.java
@@ -1,6 +1,7 @@
 package dev.efekos.arn.resolver;
 
 import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import dev.efekos.arn.handler.CommandHandlerMethod;
 import net.minecraft.commands.CommandListenerWrapper;
 
@@ -9,6 +10,6 @@ import java.lang.reflect.Parameter;
 public interface CommandHandlerMethodArgumentResolver {
 
     boolean isApplicable(Parameter parameter);
-    Object resolve(Parameter parameter, CommandHandlerMethod method, CommandContext<CommandListenerWrapper> context);
+    Object resolve(Parameter parameter, CommandHandlerMethod method, CommandContext<CommandListenerWrapper> context) throws CommandSyntaxException;
 
 }
