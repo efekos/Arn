69d1d5ddc668ee25859196c97f4c386ed2cf790e
efekos <56752400+efekos@users.noreply.github.com>
2024-05-18 00:38:58 +0300

    make argument name not required

--- src/main/java/dev/efekos/arn/annotation/CommandArgument.java
+++ src/main/java/dev/efekos/arn/annotation/CommandArgument.java
@@ -9,5 +9,5 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.PARAMETER)
 public @interface CommandArgument {
-    String value();
+    String value() default "";
 }
