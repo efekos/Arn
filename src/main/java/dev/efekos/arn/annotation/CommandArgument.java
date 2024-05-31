5710a0260e8b7b0398c85935739febfe752bb544
efekos <56752400+efekos@users.noreply.github.com>
2024-05-31 06:35:20 +0300

    remove supported type list from CommandArgument doc

--- src/main/java/dev/efekos/arn/annotation/CommandArgument.java
+++ src/main/java/dev/efekos/arn/annotation/CommandArgument.java
@@ -24,10 +24,6 @@
 
 package dev.efekos.arn.annotation;
 
-
-import dev.efekos.arn.annotation.modifier.Block;
-import dev.efekos.arn.annotation.modifier.Item;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -38,29 +34,7 @@ import java.lang.annotation.Target;
  * will try to find an applicable {@link dev.efekos.arn.resolver.CommandHandlerMethodArgumentResolver} to provide a value
  * to the parameter when calling a command handler method. If there is a {@link dev.efekos.arn.resolver.CommandArgumentResolver}
  * associated with the type of the parameter, an argument node that represents the parameter will be added to command
- * structure. As of Arn 0.1, only the following types are supported as a CommandArgument.
- * <ul>
- *     <li>{@link Boolean}</li>
- *     <li>{@link Float}</li>
- *     <li>{@link Integer}</li>
- *     <li>{@link Long}</li>
- *     <li>{@link String}</li>
- *     <li>{@link org.bukkit.block.BlockState}</li>
- *     <li>{@link org.bukkit.enchantments.Enchantment}</li>
- *     <li>{@link org.bukkit.entity.Entity}</li>
- *     <li>{@link org.bukkit.entity.Entity}[]</li>
- *     <li>{@link org.bukkit.GameMode}</li>
- *     <li>{@link org.bukkit.Location} (block position only)</li>
- *     <li>{@link org.bukkit.Material} (separated into blocks and items, see {@link Item} and {@link Block}.)</li>
- *     <li>{@link org.bukkit.World}</li>
- *     <li>{@link org.bukkit.entity.Player}</li>
- *     <li>{@link org.bukkit.entity.Player}[]</li>
- *     <li>{@link org.bukkit.potion.PotionEffectType}</li>
- *     <li>{@link net.md_5.bungee.api.chat.BaseComponent}</li>
- *     <li>{@link org.bukkit.advancement.Advancement}</li>
- *     <li>{@link org.bukkit.attribute.Attribute}</li>
- *     <li>{@link Container} {@link Enum}s that are annotated with {@link CustomArgument}.</li>
- * </ul>
+ * structure.
  *
  * @author efekos
  * @since 0.1
