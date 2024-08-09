09044b6eb1b60833ece94a562fd6f10fc2918378
efekos <56752400+efekos@users.noreply.github.com>
2024-08-09 23:20:37 +0300

    add module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module dev.efekos.arn {
+    requires brigadier;
+    requires bungeecord.chat;
+    requires org.bukkit;
+    requires org.reflections;
+    requires spigot;
+    exports dev.efekos.arn;
+}
