e8a9c1b5f611f1fc5e734cf58d7f92662465035c
efekos <56752400+efekos@users.noreply.github.com>
2024-08-14 23:45:53 +0300

    remove module-info.java (turns out that access error is cuz of java version)

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module dev.efekos.arn {
-    requires brigadier;
-    requires bungeecord.chat;
-    requires org.bukkit;
-    requires org.reflections;
-    requires spigot;
-    exports dev.efekos.arn;
-}
